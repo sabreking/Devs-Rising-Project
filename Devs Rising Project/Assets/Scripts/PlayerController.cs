9e1039b7d7385695aa5820a185c0e04686dd5d42
timo nys <timonys1998@gmail.com>
2016-03-18 22:03:33 +0100

    Controller
    Made some changes to PlayerController

--- Devs Rising Project/Assets/Scripts/PlayerController.cs
+++ Devs Rising Project/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : Player {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    Player _playerScriptRef = gameobject.GetComponent<Player>();
+    // Use this for initialization
+    void Start () {
+       
+        _playerScriptRef.hasJumped = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if(Input.GetAxis("horizontal") != 0)
+        {
+           _playerScriptRef.PlayerMove(Input.GetAxis("Horizontal"),0F);
+        }else if( Input.GetAxis("Vertical") != 0)
+        {
+            _playerScriptRef.PlayerMove(0F, Input.GetAxis("Vertical"));
+        }else if (Input.GetAxis("Jump")!= 0){
+            _playerScriptRef.PlayerJump(_playerScriptRef.hasJumped);
+            _playerScriptRef.hasJumped = true;
+        }else if (Input.GetAxis("Fire1") != 0)
+        {
+            _playerScriptRef.UseTool();
+        }
+
 	}
 }
