f09c9e1b79ccd8e6334e56b9792597b1ceee526e
stanne <resources@stannellc.com>
2016-03-18 19:32:23 -0400

    Player.cs
    Added the jump.

--- Devs Rising Project/Assets/Scripts/Player.cs
+++ Devs Rising Project/Assets/Scripts/Player.cs
@@ -5,23 +5,17 @@ public class Player : LivingEntity {
 
     //variables for PlayerMove()
     float velocity = 0.3f;
-    //public float moveHorizontal;
-    //public float moveVertical;
-
-    //private float xPos; //Player position on the x axis
-    //private float yPos; //Player position on the y axis
 
     //variables for PlayerJump()
-    bool _hasJumped;
-    public Rigidbody _rigidBody;
-    public float _jumpSpeed = 6.0f;
+    bool hasJumped;
+    Rigidbody rigidBody;
+    float jumpSpeed = 6.0f;
 
-    //variables for PlayerDeploysPlatform()
+    //variables for UseTool()
 
     // Use this for initialization
     public override void Start () {
         base.Start ();
-        //TakeDamage (23);
 	}
 	
 	// Update is called once per frame
@@ -47,8 +41,11 @@ public class Player : LivingEntity {
 
     public void PlayerJump(bool hasJumped)
     {
-        _rigidBody = GetComponent<Rigidbody>();
-        _rigidBody.AddForce(transform.up * _jumpSpeed, ForceMode.Impulse);//Based on the Unity description ForceMode.Impulse may look smoother. Try without if it doesn't look good.
+        if (hasJumped == true)
+        {
+            rigidBody = GetComponent<Rigidbody>();
+            rigidBody.AddForce(transform.up * jumpSpeed, ForceMode.Impulse);//Based on the Unity description ForceMode.Impulse may look smoother. Try without if it doesn't look good.
+        }
     }
 
     public void UseTool()
